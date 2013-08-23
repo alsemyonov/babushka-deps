8cabab2347d7462cc7fe8191326fca82208fa729
Alexander Semyonov <al@semyonov.us>
2013-08-23 00:03:17 -0700

    Setting up rbenv

---
+++ meta.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+################################################
+# © Alexander Semyonov, 2013—2013, MIT         #
+# Author: Alexander Semyonov <al@semyonov.us>  #
+################################################
+
+meta :append do
+  accepts_value_for :file
+  accepts_value_for :line
+
+  def file_name
+    File.expand_path(file)
+  end
+
+  def file_contents
+    File.readlines(file_name)
+  end
+
+  def append_file!
+    File.write(file_name, (file_contents << "#{line}\n").join(''))
+  end
+
+  template do
+    met? { file_contents.any? { |file_line| file_line =~ /#{Regexp.escape(line)}/ } }
+    meet { append_file! }
+  end
+end
---
+++ ruby.rb
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+################################################
+# © Alexander Semyonov, 2013—2013, MIT         #
+# Author: Alexander Semyonov <al@semyonov.us>  #
+################################################
+
+meta :rbenv do
+  accepts_value_for :source
+  accepts_value_for :path
+
+  def rbenv_path
+    File.expand_path('~/.rbenv')
+  end
+
+  def rbenv_plugin_path(name)
+    File.join(rbenv_path, 'plugins', name)
+  end
+
+  template do
+    requires 'git.bin'
+
+    met? { File.directory?(path) }
+    meet { git(source, to: path) }
+  end
+end
+
+dep 'rbenv installed.rbenv' do
+  source('https://github.com/sstephenson/rbenv.git')
+  path(rbenv_path)
+end
+
+dep 'ruby-build installed.rbenv' do
+  source('https://github.com/sstephenson/ruby-build.git')
+  path(rbenv_plugin_path('ruby-build'))
+end
+
+dep 'rbenv-gem-rehash installed.rbenv' do
+  source('https://github.com/sstephenson/rbenv-gem-rehash.git')
+  path(rbenv_plugin_path('rbenv-gem-rehash'))
+end
+
+dep 'rbenv-readline installed.rbenv' do
+  source('https://github.com/tpope/rbenv-readline.git')
+  path(rbenv_plugin_path('rbenv-readline'))
+end
+
+dep 'rbenv-update installed.rbenv' do
+  source('https://github.com/rkh/rbenv-update.git')
+  path(rbenv_plugin_path('rbenv-update'))
+end
+
+dep 'rbenv PATH.append', :profile do
+  file profile
+  line 'export PATH="$HOME/.rbenv/bin:$PATH"'
+end
+
+dep 'rbenv init.append', :profile do
+  file profile
+  line 'eval "$(rbenv init -)"'
+end
+
+dep 'rbenv', :profile do
+  on(:linux) { profile.default('~/.profile') }
+  on(:osx) { profile.default('~/.bash_profile') }
+
+  requires 'rbenv installed.rbenv'
+  requires 'ruby-build installed.rbenv'
+  requires 'rbenv-gem-rehash installed.rbenv'
+  requires 'rbenv-update installed.rbenv'
+  on(:osx) do
+    requires 'rbenv-readline installed.rbenv'
+  end
+  requires 'rbenv PATH.append'.with(profile)
+  requires 'rbenv init.append'.with(profile)
+end
