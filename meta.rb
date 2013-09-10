f3d4e61e0de85db419f1416a0499e173029e756c
Alexander Semyonov <al@semyonov.us>
2013-09-10 16:44:40 -0700

    Use File.open(.., "w") for 1.8.7 compatibility

--- meta.rb
+++ meta.rb
@@ -18,7 +18,7 @@ meta :append do
   end
 
   def append_file!
-    File.write(file_name, (file_contents << "#{line}\n").join(''))
+    File.open(file_name, 'w') { |f| f.write(file_contents << "#{line}\n").join('')) }
   end
 
   template do
