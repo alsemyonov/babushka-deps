8db60b5d9a2ee881fd69bcc7d84afaf0c62b5a20
Alexander Semyonov <al@semyonov.us>
2014-12-09 22:41:51 +0300

    Simplify flow

--- rails.rb
+++ rails.rb
@@ -6,14 +6,24 @@
 ################################################
 
 
-dep 'database migrated', :env, :deploying, :template => 'task' do
+dep 'database migrated', :env, :template => 'task' do
   run {
     shell "bundle exec rake db:migrate RAILS_ENV=#{env}"
   }
 end
 
-dep 'rails update', :path do
-  requires 'common:app bundled'.with(path)
-  requires 'database migrated'.with(path)
-  requires 'common:assets precompiled'.with(path)
+
+dep 'assets precompiled', :env, :template => 'task' do
+  run {
+    shell "bundle exec rake assets:precompile RAILS_GROUPS=assets RAILS_ENV=#{env}"
+  }
+end
+
+dep 'rails update', :path, :env do
+  path.default!(Dir.pwd)
+  env.default!(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production')
+
+  requires 'common:app bundled'.with(path, env)
+  requires 'database migrated'.with(env)
+  requires 'assets precompiled'.with(env)
 end
